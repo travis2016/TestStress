c479031985a8d9bf2eff2632c2bd69b21f4648ae
travsi2016 <travis2016@126.com>
2017-05-21 19:17:47 +0800

    后台更新

---
+++ src/com/szc/users/dao/UserCommonDao.java
@@ -0,0 +1,7 @@
+package com.szc.users.dao;
+
+/**
+ * 用户管理模块，共有的数据库操作方法
+ */
+public interface UserCommonDao {
+}
---
+++ src/com/szc/users/service/UserCommonService.java
@@ -0,0 +1,7 @@
+package com.szc.users.service;
+
+/**
+ * 用户管理模块，处理组的接口类
+ */
+public interface UserCommonService {
+}
